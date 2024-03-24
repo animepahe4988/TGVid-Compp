16b034838c8ad9460b1414b5d392b4eefb267073
ZxScripter <131220168+ZxScripter@users.noreply.github.com>
2024-03-24 09:58:40 +0530

    config.py

--- bot/config.py
+++ bot/config.py
@@ -9,7 +9,7 @@ try:
     BOT_TOKEN = config("BOT_TOKEN")
     DEV = 1664850827
     OWNER = config("OWNER")
-    ffmpegcode = ["-preset veryfast -c:v libx264 -s 854x480 -x264-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By Anime Sensei' -metadata:s:v title="Anime Sensei" -metadata:s:a title="Anime Sensei" -metadata:s:s title="Anime Sensei" -pix_fmt yuv420p -crf 28 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 5"]
+    ffmpegcode = ["-preset veryfast -c:v libx264 -s 854x480 -x264-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By Anime Sensei' -metadata:s:v 'title=Anime Sensei' -metadata:s:a 'title=Anime Sensei' -metadata:s:s 'title=Anime Sensei -pix_fmt yuv420p -crf 28 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 5"]
     THUMBNAIL = config("THUMBNAIL", default="https://te.legra.ph/file/86e958f9fc0d7cbdf1a28.jpg")
 except Exception as e:
     print("Environment vars Missing! Exiting App.")
